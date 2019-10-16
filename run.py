000538c9a3492bc0d0d3e166953afe94b5491f12
Arlenelalala <40139927+Arlenelalala@users.noreply.github.com>
2019-10-16 19:50:51 +0800

    修改

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from zhihu_spider.article import articles
 from process.match import keywords_match
 from save import save
 
-save_path = "/Users/liangsong/Desktop/save"
+save_path = "/save"
 # 爬取最近N天的paper，N默认为1；如果给出0，就爬取全部
 time_limit = 1
 # 可选主题，包括'自然语言处理', '机器学习', '人工智能'，'计算机视觉'，'音频语音'，'量化金融';若不给出，默认全部
